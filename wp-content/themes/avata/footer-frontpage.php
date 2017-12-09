455603098e4353b50d004df7c5bd93c0f5a99172
nithahuwaida <hafizhahuwaida@gmail.com>
2017-12-09 21:29:29 +0700

    menambah content hidro (tp belom fix)

--- wp-content/themes/avata/footer-frontpage.php
+++ wp-content/themes/avata/footer-frontpage.php
@@ -25,33 +25,33 @@
       <div class="container">
         <div class="row">
          
-          <div class="col-sm-6 col-md-6 col-lg-6">
+          <div class="col-sm-6 col-md-6 col-lg-6 text-center">
             <div class="wow fadeInRight animated" data-wow-delay="0.1s" style="visibility: visible; animation-delay: 0.1s; animation-name: fadeInRight;">
-              <div class="text-left margintop-30 avata-copyright">
+              <div class="text-center margintop-30 avata-copyright">
               <?php $copyright = wp_kses(avata_option('copyright'), $allowedposttags); ?>
-                <p><?php echo $copyright;?> &nbsp;&nbsp; <?php printf(__('Designed by <a href="%s">HooThemes</a>.','avata'),esc_url('http://www.hoothemes.com/'));?></p>
+                  <ul><p><?php echo $copyright;?> &nbsp;&nbsp; <?php printf(__('Designed by <a href="%s">KOMSI A3ND</a>.','avata'),esc_url('http://www.hoothemes.com/'));?></p></ul>
+                  <ul class="social">
+                      <?php
+                    $footer_social_icons = avata_option('footer_social_icons');
+                    foreach($footer_social_icons as $icon){
+                            $social_icon  = $icon['icon'];
+                            $social_link  = $icon['link'];
+                            $social_title = $icon['title'];
+                            $social_target = $icon['target'];
+                            $social_icon  = str_replace('fa fa-','',$social_icon);
+                            $social_icon  = str_replace('fa-','',$social_icon);
+                            $social_icon  = 'fa fa-'.trim($social_icon);
+                            
+                            if( $social_icon !=""  ){
+                            echo '<li><a href="'.esc_url($social_link).'" target="'.esc_attr($social_target).'" data-toggle="tooltip" title="'.esc_attr($social_title).'"><i class="'.esc_attr($social_icon).'"></i></a></li>';
+                            }
+                            }
+                            ?>
+                              </ul>       
               </div>
             </div>
           </div>
-           <div class="col-sm-6 col-md-6 col-lg-6 text-right">
-            <ul class="social">
-    <?php
-	$footer_social_icons = avata_option('footer_social_icons');
-	foreach($footer_social_icons as $icon){
-					$social_icon  = $icon['icon'];
-					$social_link  = $icon['link'];
-					$social_title = $icon['title'];
-					$social_target = $icon['target'];
-					$social_icon  = str_replace('fa fa-','',$social_icon);
-					$social_icon  = str_replace('fa-','',$social_icon);
-					$social_icon  = 'fa fa-'.trim($social_icon);
-					
-					if( $social_icon !=""  ){
-					echo '<li><a href="'.esc_url($social_link).'" target="'.esc_attr($social_target).'" data-toggle="tooltip" title="'.esc_attr($social_title).'"><i class="'.esc_attr($social_icon).'"></i></a></li>';
-					}
-					}
-					?>
-            </ul>       
+           
           </div>
         </div>
       </div>
